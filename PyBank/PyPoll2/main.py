ac1d75b3ecc0cb3baa8049b96882c92cbda06c44
LPELLITTERI <lmo0o@yahoo.com>
2023-02-18 12:09:36 -0500

    changed code still running with errors

--- PyBank/PyPoll2/main.py
+++ PyBank/PyPoll2/main.py
@@ -1,74 +1,59 @@
 import os
 import csv
 
-def read_csv(csv_path, delimiter=',', has_header=True):
-    """Reads a CSV file and returns a list of rows.
+# Set file paths
+csv_path = os.path.join('Resources', 'election_data.csv')
+output_path = os.path.join('Analysis.txt')
 
-    Args:
-        csv_path (str): The file path to the CSV file.
-        delimiter (str, optional): The delimiter used in the file. Defaults to ','.
-        has_header (bool, optional): Whether or not the file has a header row. Defaults to True.
-
-    Returns:
-        list: A list of rows from the CSV file.
-    """
+def read_csv(csv_path):
+    """Reads a CSV file and returns a list of rows."""
     with open(csv_path) as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=delimiter)
-        if has_header:
-            header = next(csvreader)
-        else:
-            header = None
-        rows = []
-        for row in csvreader:
-            rows.append(row)
-        return header, rows
+        csvreader = csv.reader(csvfile, delimiter=',')
+        header = next(csvreader)
+        rows = [row for row in csvreader]
+    return header, rows
 
-
-def count_candidate_votes(data):
-    """Counts the number of votes each candidate received.
-
-    Args:
-        data (list): A list of rows from the CSV file.
-
-    Returns:
-        dict: A dictionary containing the candidate names and their vote counts.
-    """
-    candidate_vote_count = {}
-    for row in data:
+def count_candidate_votes(rows):
+    """Counts the number of votes each candidate received."""
+    vote_counts = {}
+    for row in rows:
         candidate_name = row[2]
-        if candidate_name in candidate_vote_count:
-            candidate_vote_count[candidate_name] += 1
+        if candidate_name in vote_counts:
+            vote_counts[candidate_name] += 1
         else:
-            candidate_vote_count[candidate_name] = 1
-    return candidate_vote_count
+            vote_counts[candidate_name] = 1
+    return vote_counts
 
+def calculate_results(vote_counts):
+    """Calculates and returns the election results."""
+    total_votes = sum(vote_counts.values())
+    winner = max(vote_counts, key=vote_counts.get)
+    results = []
+    for candidate, votes in vote_counts.items():
+        percentage = votes / total_votes * 100
+        results.append((candidate, votes, percentage))
+    return total_votes, winner, results
 
-def print_election_results(total_votes, vote_counts):
-    """Prints the election results.
-
-    Args:
-        total_votes (int): The total number of votes cast.
-        vote_counts (dict): A dictionary containing the candidate names and their vote counts.
-    """
-    line = "---------------"
-    print(line)
-    print("ELECTION RESULTS")
-    print(line)
-    print(f"Total Votes: {total_votes}")
-    print(line)
-    winning_total = 0
-    for key, value in vote_counts.items():
-        print(f"{key}: {value} ({round((value/total_votes) * 100, 3)}%)")
-        if value > winning_total:
-            winning_total = value
-            winning_candidate = key
-    print(line)
-    print(f"WINNER: {winning_candidate} with {winning_total} votes")
-
+def format_results(total_votes, winner, results):
+    """Formats the election results for printing."""
+    output = []
+    output.append("ELECTION RESULTS")
+    output.append("-------------------------")
+    output.append(f"Total Votes: {total_votes}")
+    output.append("-------------------------")
+    for candidate, votes, percentage in results:
+        output.append(f"{candidate}: {percentage:.3f}% ({votes})")
+    output.append("-------------------------")
+    output.append(f"Winner: {winner}")
+    output.append("-------------------------")
+    return "\n".join(output)
 
 if __name__ == '__main__':
-    csv_path = os.path.join('Resources', 'election_data.csv')
-    header, data = read_csv(csv_path, has_header=True)
-    vote_counts = count_candidate_votes(data)
-    total_votes = sum(vote_counts.values())
-    print_election_results(total_votes, vote_counts)
+    header, rows = read_csv(csv_path)
+    vote_counts = count_candidate_votes(rows)
+    total_votes, winner, results = calculate_results(vote_counts)
+    output = format_results(total_votes, winner, results)
+    print(output)
+
+    with open(output_path, 'w') as file:
+        file.write(output)
